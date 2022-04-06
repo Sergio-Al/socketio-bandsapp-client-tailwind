bf66e756e55b356033e3c1f927781d7670125066
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-04-05 21:08:14 -0400

    fixing dialog position

--- src/components/AddDialog.js
+++ src/components/AddDialog.js
@@ -4,10 +4,10 @@ import AddForm from "./AddForm";
 const AddDialog = ({ disableDialog }) => {
   return (
     <div
-      className="bg-slate-500 absolute inset-0 bg-opacity-80 text-white flex justify-center items-center animate-fade"
+      className="bg-slate-500 fixed min-h-full inset-0 bg-opacity-80 text-white flex justify-center items-center animate-fade"
       onClick={() => disableDialog()}
     >
-      <AddForm disableDialog={disableDialog}/>
+      <AddForm disableDialog={disableDialog} />
     </div>
   );
 };
